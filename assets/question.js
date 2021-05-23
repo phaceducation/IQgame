c43653e941a35b97dc1d30d0ec2a44d4dfe09fc8
phaceducation <84669588+phaceducation@users.noreply.github.com>
2021-05-23 18:52:32 +0545

    Add files via upload

---
+++ assets/question.js
@@ -0,0 +1,62 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which one of the five is least like the other four:",
+    answer: "Football",
+    options: [
+      "Billiards",
+      "Baseball",
+      "Golf",
+      "Football"
+    ]
+  },
+    {
+    numb: 2,
+    question: "OCEAN is to AENCO as 89653 is to:",
+    answer: "56398",
+    options: [
+      "85369",
+      "56389",
+      "56398",
+      "65398"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which one of the number doesnot belongs in the following series : 2-3-6-7-8-14-15-30",
+    answer: "8",
+    options: [
+      "8",
+      "14",
+      "15",
+      "7"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Virus is to Vaccine as Exam is to :",
+    answer: "Studying",
+    options: [
+      "Trying ",
+      "Passing",
+      "Studying",
+      "Falling"
+    ]
+  },
+    {
+    numb: 5,
+    question: "If there are 3 oranges and you take away 2, how many do you have?",
+    answer: "2 Oranges",
+    options: [
+      "0 Oranges",
+      "2 Oranges",
+      "3 Oranges",
+      "1 Oranges"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+ 
+  
+];
